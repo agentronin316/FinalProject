804892188cd1114ed3112ae8aebc34af6efe751b
agentronin316 <agentronin316@gmail.com>
2015-12-09 14:56:39 -0600

    Enemies now find each other
    Also created data class for claws and reworked a bit of the folder
    structure.

---
+++ RailEngine/Assets/Scripts/DataScripts/ScriptClawData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptClawData : MonoBehaviour
+{
+
+	public float range;
+	public float moveSpeed;
+	public float storageVolume;
+	public float transferDelay;
+}
--- RailEngine/Assets/Scripts/ScriptEnemyMovement.cs
+++ RailEngine/Assets/Scripts/ScriptEnemyMovement.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 public class ScriptEnemyMovement : MonoBehaviour {
 
     GameObject[] players;
     ScriptEnemyData data;
 
+    Vector3 newPos;
+    bool alive = true;
+    bool posUpdated = false;
+
+    List<GameObject> nearbyEnemies = new List<GameObject>();
 	// Use this for initialization
 	void Start ()
     {
+        newPos = transform.position;
         players = GameObject.FindGameObjectsWithTag("Player");
         data = gameObject.GetComponent<ScriptEnemyData>();
+        InitiateAI();
+        if (data.aiType != EnemyAIType.STATIONARY)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 20f);
+            foreach (Collider col in colliders)
+            {
+                if (col.gameObject.transform.tag == "Enemy" && col.gameObject != this.gameObject)
+                {
+                    nearbyEnemies.Add(col.gameObject);
+                }
+                
+            }
+        }
 	}
 	
-	// Update is called once per frame
-	void Update ()
+    void Update()
     {
-        bool awake = false;
+        transform.position = newPos;
+        posUpdated = true;
+    }
 
+	void InitiateAI ()
+    {
+        Thread thread;
         switch (data.aiType)
         {
             case EnemyAIType.STATIONARY:
@@ -25,36 +50,69 @@ public class ScriptEnemyMovement : MonoBehaviour {
                 break;
 
             case EnemyAIType.CONCEALING:
-                foreach (GameObject player in players)
-                {
-                    if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
-                    {
-                        awake = true;
-                    }
-                }
-
-                if (awake)
-                {
-                    //Find nearby higher value "enemy"
-                    //Move in front of it
-                }
+                thread = new Thread(AIConceal);
+                thread.Start();
                 break;
                 
             case EnemyAIType.HIDING:
-                foreach (GameObject player in players)
-                {
-                    if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
-                    {
-                        awake = true;
-                    }
-                }
-
-                if (awake)
-                {
-                    //Find nearby lower value "enemy"
-                    //Move behind it
-                }
+                thread = new Thread(AIHide);
+                thread.Start();
                 break;
         }
 	}
+
+    void AIConceal()
+    {
+        Stopwatch timer = new Stopwatch();
+        long oldTime = timer.ElapsedMilliseconds;
+        while (alive)
+        {
+            bool awake = false;
+            foreach (GameObject player in players)
+            {
+                if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
+                {
+                    awake = true;
+                }
+            }
+            if (posUpdated)
+            {
+                if (awake)
+                {
+                    float tempTime = (timer.ElapsedMilliseconds - oldTime) / 1000f;
+                    //Find nearby higher value "enemy"
+                    //Move in front of it
+                    timer = new Stopwatch();
+                }
+                else
+                {
+                    timer = new Stopwatch();
+                }
+                posUpdated = false;
+            }
+            Thread.Sleep(30);
+        }
+    }
+
+    void AIHide()
+    {
+        while (alive)
+        {
+            bool awake = false;
+            foreach (GameObject player in players)
+            {
+                if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
+                {
+                    awake = true;
+                }
+            }
+
+            if (awake)
+            {
+                //Find nearby lower value "enemy"
+                //Move behind it
+            }
+            Thread.Sleep(30);
+        }
+    }
 }

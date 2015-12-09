32635c37bae314707f3de11495228c07e530659a
agentronin316 <agentronin316@gmail.com>
2015-12-09 08:41:08 -0600

    Framed out Enemy AI
    Also added in a data structure class for enemies.

---
+++ RailEngine/Assets/Scripts/ScriptEnemyData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyAIType
+{
+    STATIONARY,
+    HIDING,
+    CONCEALING
+}
+
+public class ScriptEnemyData : MonoBehaviour {
+
+    public EnemyAIType aiType = EnemyAIType.STATIONARY;
+    public float moveSpeed = 0;
+    public bool collectible = true;
+    public int pointValue = 100;
+    public float awakeDistance = 40f;
+}
---
+++ RailEngine/Assets/Scripts/ScriptEnemyMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptEnemyMovement : MonoBehaviour {
+
+    GameObject[] players;
+    ScriptEnemyData data;
+
+	// Use this for initialization
+	void Start ()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        data = gameObject.GetComponent<ScriptEnemyData>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool awake = false;
+
+        switch (data.aiType)
+        {
+            case EnemyAIType.STATIONARY:
+                //Do nothing
+                break;
+
+            case EnemyAIType.CONCEALING:
+                foreach (GameObject player in players)
+                {
+                    if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
+                    {
+                        awake = true;
+                    }
+                }
+
+                if (awake)
+                {
+                    //Find nearby higher value "enemy"
+                    //Move in front of it
+                }
+                break;
+                
+            case EnemyAIType.HIDING:
+                foreach (GameObject player in players)
+                {
+                    if ((player.transform.position - transform.position).magnitude > data.awakeDistance)
+                    {
+                        awake = true;
+                    }
+                }
+
+                if (awake)
+                {
+                    //Find nearby lower value "enemy"
+                    //Move behind it
+                }
+                break;
+        }
+	}
+}
